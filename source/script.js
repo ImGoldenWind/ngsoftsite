b29080e936adb33ae04818d3d37d4d284f0792b2
ImGoldenWind <66796221+ImGoldenWind@users.noreply.github.com>
2024-09-12 15:50:39 +0300

    Update script.js

--- source/script.js
+++ source/script.js
@@ -1,46 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const interBubble = document.querySelector(".interactive");
+  let curX = 0;
+  let curY = 0;
+  let tgX = 0;
+  let tgY = 0;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const interBubble = document.querySelector('.interactive');
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
+  const move = () => {
+    curX += (tgX - curX) / 20;
+    curY += (tgY - curY) / 20;
+    interBubble.style.transform = `translate(${Math.round(
+      curX
+    )}px, ${Math.round(curY)}px)`;
+    requestAnimationFrame(move);
+  };
 
-    const move = () => {
-        curX += (tgX - curX) / 20;
-        curY += (tgY - curY) / 20;
-        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(move);
-    };
+  window.addEventListener("mousemove", (event) => {
+    tgX = event.clientX;
+    tgY = event.clientY;
+  });
 
-    window.addEventListener('mousemove', (event) => {
-        tgX = event.clientX;
-        tgY = event.clientY;
-    });
-
-    move();
+  move();
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var contactButton = document.getElementById('contact-us-button');
-    var contactMenuItem = document.getElementById('contact-us-link');
-    var servicesButton = document.getElementById('services-button');
-    var targetContactElement = document.getElementById('contact-us');
-    var targetServicesElement = document.getElementById('services-info-id');
+document.addEventListener("DOMContentLoaded", function () {
+  var contactButton = document.getElementById("contact-us-button");
+  var contactMenuItem = document.getElementById("contact-us-link");
+  var servicesButton = document.getElementById("services-button");
+  var targetContactElement = document.getElementById("contact-us");
+  var targetServicesElement = document.getElementById("services-info-id");
+  var projectsButton = document.getElementById("our-projects-button");
+  var targetProjectslement = document.getElementById("our-project-info-id");
 
-    // Обработчик для кнопки "Связаться с нами"
-    contactButton.addEventListener('click', function () {
-        targetContactElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    });
+  contactButton.addEventListener("click", function () {
+    targetContactElement.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
 
-    // Обработчик для элемента меню "Связаться с нами"
-    contactMenuItem.addEventListener('click', function () {
-        targetContactElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    });
+  contactMenuItem.addEventListener("click", function () {
+    targetContactElement.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
 
-    // Обработчик для кнопки "Наши услуги"
-    servicesButton.addEventListener('click', function () {
-        targetServicesElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    });
+  projectsButton.addEventListener("click", function () {
+    targetProjectslement.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
 });
-
