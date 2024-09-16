b196dc6c06dd0c77aac1dd6e54dc948b5adc1a2b
ImGoldenWind <66796221+ImGoldenWind@users.noreply.github.com>
2024-09-16 16:14:33 +0300

    Update script.js

--- source/script.js
+++ source/script.js
@@ -43,3 +43,25 @@ document.addEventListener("DOMContentLoaded", function () {
     targetProjectslement.scrollIntoView({ behavior: "smooth", block: "start" });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 10000,
+      disableOnInteraction: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    loop: true,
+  });
+
+  console.log("Swiper initialized:", swiper);
+});
