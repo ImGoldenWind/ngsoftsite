88e4a36e3637c75510ee3e28f27cee7d3cacacfc
ImGoldenWind <66796221+ImGoldenWind@users.noreply.github.com>
2024-09-20 03:04:14 +0300

    Update script.js

--- source/script.js
+++ source/script.js
@@ -28,8 +28,21 @@ document.addEventListener("DOMContentLoaded", function () {
   var servicesButton = document.getElementById("services-button");
   var targetContactElement = document.getElementById("contact-us");
   var targetServicesElement = document.getElementById("services-info-id");
+  var aboutusButton = document.getElementById("about-us-button");
+  var aboutusectslement = document.getElementById("about-us-link");
   var projectsButton = document.getElementById("our-projects-button");
   var targetProjectslement = document.getElementById("our-project-info-id");
+
+  aboutusButton.addEventListener("click", function () {
+    aboutusectslement.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
+
+  servicesButton.addEventListener("click", function () {
+    targetServicesElement.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
 
   contactButton.addEventListener("click", function () {
     targetContactElement.scrollIntoView({ behavior: "smooth", block: "start" });
@@ -64,4 +77,54 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   console.log("Swiper initialized:", swiper);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const lines = document.querySelectorAll(".editor-line");
+  let typingStarted = false;
+
+  function typeWriter(element, charIndex, callback) {
+    const text = element.getAttribute("data-text");
+    if (charIndex < text.length) {
+      element.textContent = text.substring(0, charIndex + 1);
+      setTimeout(() => typeWriter(element, charIndex + 1, callback), 50);
+    } else if (callback) {
+      callback();
+    }
+  }
+
+  function startTypingEffect(index) {
+    if (index >= lines.length) return;
+
+    const element = lines[index];
+    element.style.opacity = 1;
+
+    typeWriter(element, 0, () => {
+      startTypingEffect(index + 1);
+    });
+  }
+
+  function handleIntersection(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && !typingStarted) {
+        typingStarted = true;
+        startTypingEffect(0);
+        observer.disconnect();
+      }
+    });
+  }
+
+  // Создаем объект Intersection Observer
+  const observer = new IntersectionObserver(handleIntersection, {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0, 
+  });
+
+  observer.observe(lines[0]);
+
+  lines.forEach((line) => {
+    line.setAttribute("data-text", line.textContent);
+    line.textContent = ""; 
+  });
 });
